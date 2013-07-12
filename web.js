8e35fdb007615873443f72253349f3535de8f0ad
ArtjomKorol <timo261188@gmail.com>
2013-07-12 20:24:59 +0000

    Update on index.html reponse 2

--- web.js
+++ web.js
@@ -2,15 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var text = fs.readFileSync('index.html','utf-8');
-var buffer = new Buffer('text','utf-8');
-var reply = buffer.toString('utf-8', 0, 27);
-
 app.get('/', function(request, response) {
-  response.send(reply);
+    var fs = require('fs');
+    var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
